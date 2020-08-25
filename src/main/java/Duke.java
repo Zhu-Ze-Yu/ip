b4146aa7ae55612d4b715470f4fc545d5b054e88
Zhu-Zeyu <20001115zhuzeyu@gmail.com>
2020-08-25 19:53:04 +0800

    level 2 Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,9 +13,11 @@ public class Duke {
         System.out.println("     What can I do for you?");
         printLine();
 
-        while(true) {
+        String[] sentences = new String[100];
+        for(int i=0; i>=0; i++) {
             Scanner in = new Scanner(System.in);
             String words = in.nextLine();
+            sentences[i] = words;
             printLine();
 
             if (words.equals("bye")) {
@@ -24,8 +26,21 @@ public class Duke {
                 return;
             }
 
-            System.out.println("     " + words);
-            printLine();
+            else if (words.equals("list")) {
+                int num = 1;
+                for (String sentence: sentences) {
+                    if (sentence.equals("list")) break;
+                    System.out.println("     " + num + ". " + sentence);
+                    num++;
+                }
+                printLine();
+                i--;
+            }
+
+            else {
+                System.out.println("     added: " + words);
+                printLine();
+            }
         }
     }
 }
