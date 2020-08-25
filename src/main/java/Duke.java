b292ad5ca3eba27201104c0f60f95caee557b2bc
Zhu-Zeyu <20001115zhuzeyu@gmail.com>
2020-08-25 20:20:53 +0800

    level 3 Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,11 +13,12 @@ public class Duke {
         System.out.println("     What can I do for you?");
         printLine();
 
-        String[] sentences = new String[100];
-        for(int i=0; i>=0; i++) {
+        String[] taskName = new String[100];
+        String[] taskStatus = new String[100];
+        int countTask = 0;
+        for(int i=0; i<100; i++) {
             Scanner in = new Scanner(System.in);
             String words = in.nextLine();
-            sentences[i] = words;
             printLine();
 
             if (words.equals("bye")) {
@@ -27,17 +28,29 @@ public class Duke {
             }
 
             else if (words.equals("list")) {
-                int num = 1;
-                for (String sentence: sentences) {
-                    if (sentence.equals("list")) break;
-                    System.out.println("     " + num + ". " + sentence);
-                    num++;
+                System.out.println("     Here are the tasks in your list:");
+
+                int taskNum;
+                for (int j=0; j<countTask; j++) {
+                    taskNum = j + 1;
+                    System.out.println("     " + taskNum + ".[" + taskStatus[j] + "] " + taskName[j]);
                 }
                 printLine();
-                i--;
+            }
+
+            else if (words.startsWith("done") && words.length() ==6) {
+                System.out.println("     Nice! I've marked this task as done:");
+                String taskIndex = words.substring(5,6);
+                int index = Integer.parseInt(taskIndex);
+                taskStatus[index-1] = "\u2713";
+                System.out.println("       " + "[\u2713] " + taskName[index-1]);
             }
 
             else {
+                taskName[countTask] = words;
+                taskStatus[countTask] = "\u2718";
+                countTask++;
+
                 System.out.println("     added: " + words);
                 printLine();
             }
